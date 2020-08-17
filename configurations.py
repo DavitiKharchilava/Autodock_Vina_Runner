87d60cfe1e62e3edeadec4c10a0fbae1ad4ef47d
unknown <DavitiKharchilava>
2020-08-17 05:15:55 +0400

    added output_dir creating options, updated README.md, added detailed comments

--- configurations.py
+++ configurations.py
@@ -5,50 +5,66 @@ while True:
 
         parser = argparse.ArgumentParser(add_help=False, description='Runs Autodock Vina multi-ligand docking!')
 
-        # Add the arguments
+        ## Add the arguments:
         parser.add_argument("inputProteinName",
                             type=str,
                             help="->> python3 vina_runner.py [Protein Name Here] <<- "
                                  "Here is the example of a command. "
                                  "You must type correct protein name to run the docking! ")
 
+        ## Optional Arguments:
         parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
                             help="In the command line you need to type the name of a protein correctly (after the "
                                  "'python3 vina_runner.py ') to let the script start the docking with Autodock Vina. "
                                  "The written protein name represents the conf_PROTEIN.txt config file which should "
                                  "be located in the same directory as 'vina_runner.py' and it also should be set up "
                                  "properly for docking. Have a happy docking!")
-        # parser.add_argument('-o', action="store", type=str)
+        parser.add_argument('-o', "--output", help="Output directory path. It is optional argument, "
+                                                   "default path for output directory should be already mentioned "
+                                                   "in conf_[PROTEIN].txt file, by user.", action="store", type=str)
 
-        # Execute the parse_args() method
+        ## Execute the parse_args() method:
         args = parser.parse_args()
 
         pName = args.inputProteinName
 
-        conf_name = "conf_" + pName + ".txt"    # conf_[PROTEIN_NAME].txt should be located in the same directory as
-                                                # configuration.py and vina_runner.py
+        conf_name = "conf_" + pName + ".txt"    ## conf_[PROTEIN_NAME].txt must be located in the same directory as
+                                                ## configuration.py and vina_runner.py
         with open(conf_name) as file_in:
             lines = []
             for line in file_in:
                 lines.append(line)
 
-        # ligdir = lines[0][9:-1]
-        ligpath = lines[1][10:].rstrip()
-        receptor = lines[2][11:].rstrip()
-        freceptor = lines[3][12:].rstrip()
-        vina_dir = r"C:\Program1\TSRI\Vina\vina.exe"  # edit this line if vina.exe installation directory needs to be changed
-        output_dir = lines[5][13:-1].rstrip()
+        ## Slicing each line of data:
 
+        ## Directory were all ligands area located for docking:
+        ligpath = lines[1][10:].rstrip()
+        ## Protein file name:
+        receptor = lines[2][11:].rstrip()
+        ## Protein file full path:
+        freceptor = lines[3][12:].rstrip()
+
+        #####!!!!! edit this line below , if vina.exe installation directory needs to be changed !!!!!#####
+        vina_dir = r"C:\Program1\TSRI\vina\vina.exe"
+
+        ## Accepts output directory from command -o [out_dir]:
+        if args.output is not None:
+            print("\nSetting up output_dir path manually...")
+            output_dir = args.output
+        ## If from command line output_dir is not set, it uses default option from config_[PROTEIN].txt file:
+        else:
+            print("\nUsing default output_dir path from " + conf_name)
+            output_dir = lines[5][13:-1].rstrip()
+        ## Checks if conf_[PROTEIN].txt file exists.
+        if os.path.isfile(conf_name) is True:
+            print("\nConfig file name is correct!")
+        ## Checks if directory already exists, if not creates one:
         if not os.path.isdir(output_dir):
             os.mkdir(output_dir)
         else:
             output_dir = output_dir
 
-        # output_dir = str(args)[38:-2]
-        # if output_dir == '':
-        #     output_dir = lines[5][13:-1].rstrip()
-
-
+        ## Autodock Vina's configuration details that user have set-up in conf_[PROTEIN].txt file:
         strcenter_x = lines[6][14:].rstrip()
         strcenter_y = lines[7][14:].rstrip()
         strcenter_z = lines[8][14:].rstrip()
@@ -59,34 +75,10 @@ while True:
         strexhaustiveness = lines[13][20:].rstrip()
         strnum_modes = lines[14][15:].rstrip()
 
-        conf_path = lines[15][12:].rstrip() + conf_name
-
-        # conf_path = "X:/Documents/Python/TurboVina-master/vina_runner/" + conf_name
-        conf_file = open(conf_path)
-
-        if os.path.isfile(conf_path) is True:
-            print("Config file name is correct!")
-        else:
-            print('Wrong protein name!')
-    #
-    #     # ligdir = "C:\\Program1\\TSRI\\Vina\\Ligands\\ligs"  # should be changed for ligands directory
-    #     # ligpath = "C:\\Program1\\TSRI\\Vina\\Ligands\\ligs"  # should be changed for ligands directory
-    #     # receptor = "6m71.pdbqt"  # receptor file name
-    #     # freceptor = str("C:\\Program1\\TSRI\\Vina\\" + receptor)  # contains receptor location + receptor file name
-    #     # vina_dir = r"C:\Program1\TSRI\Vina\vina.exe"  # if the vina.exe installed directory needs to be changed, change only r'YOUR_DIRECTORY'.
-    #     # output = "C:\\Program1\\TSRI\\Vina\\test2\\"  # defines docking output directory
-    #     #
-    #     # # Autodock Vina docking parameters:
-    #     # strcenter_x = str(146)
-    #     # strcenter_y = str(124)
-    #     # strcenter_z = str(91)
-    #     # strsize_x = str(17)
-    #     # strsize_y = str(13)
-    #     # strsize_z = str(13)
-    #     # strcpu = str(4)
-    #     # strexhaustiveness = str(8)
-    #     # strnum_modes = str(9)
+        ## Breaks out of while loop if try-ing if the script was working
         break
+
     except FileNotFoundError:
-        print("\nWrong protein name! Script cannot find the config file! type -> python3 vina_runner.py --help")
+        print("\nWrong protein name! Script cannot find the config file! type -> python vina_runner.py --help")
+        ## Breaks out of while loop if script was crashed for 'FileNotFoundError'
         break
--- vina_runner.py
+++ vina_runner.py
@@ -1,30 +1,27 @@
 import os, subprocess, configurations, argparse, sys
+
+# texttable package should be installed for better looking output results:
 from texttable import Texttable
 
 while True:
     try:
-        # CALLING VINA
-
+        ## Looping through the names of a ligands located to the ligands directory and saving them in a list 'lignames':
         lignames = []
         for file in os.listdir(configurations.ligpath):
             if file.endswith(".pdbqt"):
                 lignames.append(file)
 
+        ## Looping through each 'lignames' list member and preparing command for Autodock Vina docking:
         for i in range(len(lignames)):
             fligand = str('"' + configurations.ligpath + "\\" + lignames[i] + '"')
 
-            # print_path = ''
-            # for i in range(len(configurations.output_dir)):
-            #     if configurations.output_dir[i] == "\\" and configurations.output_dir[i+1] == "\\":
-            #         print_path = configurations.output_dir[:i] \
-            #                      + configurations.output_dir[i] \
-            #                      + configurations.output_dir[i+1:0]
-
+            ## Telling the names of a ligand and a protein which are going to be docked, output directory is mentioned too:
             print("\nNow will be docked ligand "
                   + "[" + lignames[i][:-6] + "]" + " to a protein "
                   + "[" + configurations.receptor[:-6] + "]"
                   + ". \nOutput directory for docking will be -->> " + configurations.output_dir)
 
+            ## CALLING VINA AND ADDING ALL THE CONFIGURATIONS:
             subprocess.call(
                 configurations.vina_dir + " --receptor " + configurations.freceptor + " --ligand " + fligand \
                 + " --center_x " + configurations.strcenter_x \
@@ -40,10 +37,12 @@ while True:
                 + "_" + "out" + ".pdbqt")
             i += 1
 
+            ## Telling how many ligands are located in ligand(s)' directory:
             path_lig, dirs_lig, files_lig = next(os.walk(configurations.ligpath))
             file_count_lig = str(len(files_lig))
             print("\n" + "The number of ligand(s) in the ligand(s)' directory: " + file_count_lig)
 
+            ## Telling how many dockings were successfully completed:
             path_out, dirs_out, files_out = next(os.walk(configurations.output_dir))
             file_count_out = str(len(files_out))
             print("The number of successfully docked ligands: " + file_count_out)
@@ -52,71 +51,50 @@ while True:
             """For sorting Autodock Vina outputs"""
 
             print("\n\nSorted docking output with binding energies from highest to low:")
-            try:
-                # from texttable import Texttable
 
-                # macromolecule = input("Please enter the name of a protein: ").lower()
+            ## If you choose to save the output in .txt file [all data.txt] too, the comments "#" need to be
+            ## removed in lines where the text_file is mentioned
 
-                # output_path = "C:/Program1/TSRI/vina/test2"  # + macromolecule
+            # text_file = open(output_path + "/all data.txt", mode="a")
 
-                """ If you choose to save the output in .txt file [all data.txt] too, the comments "#" need to be removed in lines where
-                the text_file is mentioned """
+            list_pdbqt = []
 
-                # text_file = open(output_path + "/all data.txt", mode="a")
+            for filename in os.listdir(configurations.output_dir):
+                if filename.endswith(".pdbqt"):  # "out" should be deleted if we sort with filenames
+                    pdbqt_file = open(configurations.output_dir + "/" + filename, mode="r")
+                    pdbqt_file.readline()
+                    new_content = pdbqt_file.readline()
+                    list_pdbqt.append((filename, new_content))
+                else:
+                    print("sorting was not successful, ")
 
-                list_pdbqt = []
+            list_pdbqt = sorted(list_pdbqt, key=lambda f: float(f[1].split()[3]))
+            ## filename: filename <- for sorting with names
+            ## f: float(f[1].split()[3]) <- for sorting with energy
 
-                for filename in os.listdir(configurations.output_dir):
-                    if filename.endswith(".pdbqt"):  # "out" should be deleted if we sort with filenames
-                        pdbqt_file = open(configurations.output_dir + "/" + filename, mode="r")
-                        pdbqt_file.readline()
-                        new_content = pdbqt_file.readline()
-                        list_pdbqt.append((filename, new_content))
-                    else:
-                        print("sorting was not successful, ")
+            t = Texttable()
 
-                list_pdbqt = sorted(list_pdbqt, key=lambda f: float(f[1].split()[3]))
-                # filename: filename -for sorting with names
-                # f: float(f[1].split()[3]) -for sorting with energy
+            for (filename, new_content) in list_pdbqt:
+                # text_file.write(filename)
+                # text_file.write("\n")
+                # text_file.write(new_content)
+                # text_file.write("\n")
+                # text_file.flush()
 
-                t = Texttable()
+                ## If you choose to see the output without texttable package:
+                # print(filename, "||", new_content.split()[3], "||", configurations.output_dir "\n")
 
-                for (filename, new_content) in list_pdbqt:
-                    # text_file.write(filename)
+                ## Reviewing sorted output with 'texttable' package:
+                t.add_rows([['Output file name', 'The strongest binding energy', 'Output Directory Path'],
+                            [filename, new_content.split()[3], configurations.output_dir]])
 
-                    # x = PrettyTable()
-                    # x.field_names = ["Docked Ligand Output Name", "Binding Energy", "Output Directory Path"]
-                    # x.add_row([filename, new_content.split()[3], configurations.output_dir])
-                    # print(x)
+            ## Printing output texttable:
+            print(t.draw())
 
-                    # text_file.write("\n")
-                    # text_file.write(new_content)
-                    # text_file.write("\n")
-                    t.add_rows([['Output file name', 'The strongest binding energy', 'Output Directory Path'],
-                                [filename, new_content.split()[3], configurations.output_dir]])
-
-                    # text_file.flush()
-                print(t.draw())
-            except:
-                list_pdbqt = []
-
-                for filename in os.listdir(configurations.output_dir):
-                    if filename.endswith(".pdbqt"):  # "out" should be deleted if we sort with filenames
-                        pdbqt_file = open(configurations.output_dir + "/" + filename, mode="r")
-                        pdbqt_file.readline()
-                        new_content = pdbqt_file.readline()
-                        list_pdbqt.append((filename, new_content))
-                    else:
-                        print("sorting was not successful, ")
-
-                list_pdbqt = sorted(list_pdbqt, key=lambda f: float(f[1].split()[3]))
-                print("Something went wrong while sorting output files ! Introducing alternative sorted output:\n")
-                print("\nOutput file name || The strongest binding energy || Output Directory Path")
-                for (filename, new_content) in list_pdbqt:
-                    print("\n" + filename + " || " + new_content.split()[3] + " || " + configurations.output_dir + "\n")
-                """_________________________________________________________"""
+            """_________________________________________________________"""
 
         break
     except:
+        ## In case 'vina_runner.py' crashes:
         print("\n\n'vinna_runner.py' stopped working!\n")
         break
